0078c4bb9fb5b387c5c121406a4bb0e2f324c3ba
DoykFilt <thibaud.beaufils@gmail.com>
2017-03-28 15:26:29 +0200

    Initialisation du projet

---
+++ src/tpnotesynchro/Algorithm.java
@@ -0,0 +1,20 @@
+package tpnotesynchro;
+
+import java.util.Properties;
+
+/**
+ * Defines the interface to optimization algorithms
+ * @author Jorge E. Mendoza (dev@jorge-mendoza.com)
+ * @version %I%, %G%
+ *
+ */
+public interface Algorithm {
+	
+	/**
+	 * Runs the algorithm and returns the solution
+	 * @param config the algorithm's configuration
+	 * @return the solution to the problem
+	 */
+	public Solution run(Properties config);
+	
+}
